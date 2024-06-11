4e5a4d07c5d73c9d64db434a2bbb55c0ca574f84
Manuel Madera <maderamanuel25@email.com>
2024-06-10 21:10:15 -0400

    Update

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pagina-principal.inicio');
+    return view('auth.login');
 });
 
 Route::get('/contratoInquilinato', function () {
